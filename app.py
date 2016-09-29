29a16c98db2a49724112c57255dd82eebdcfb34b
iamrajhans <rajhans.jadhao@gmail.com>
2016-09-29 17:04:10 +0530

    added index.html file

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def hello():
-    return "hello"
+    return render_template('index.html')
 
 app.run()
